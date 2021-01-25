6837b5bd2d50ab75f0c4de4d28278284f004e78c
Stoyko Stoykov <stoyko.stoykov@pulselive.com>
2021-01-25 19:44:19 +0200

    setup crons

--- cron/cron.py
+++ cron/cron.py
@@ -8,37 +8,37 @@ scheduler = APScheduler()
 atexit.register(lambda: scheduler.shutdown())
 
 
-@scheduler.task('cron', minute='0', hour="*/4")
+@scheduler.task('cron', minute='0', hour='*/4')
 def job_scrap_kraken_btc_usd():
     with scheduler.app.app_context():
         scrap_kraken('XXBTZUSD')
 
 
-@scheduler.task('cron', minute='1', hour="*/4")
+@scheduler.task('cron', minute='1', hour='*/4')
 def job_scrap_kraken_etg_usd():
     with scheduler.app.app_context():
         scrap_kraken('XETHZUSD')
 
 
-@scheduler.task('cron', minute='2', hour="*/4")
+@scheduler.task('cron', minute='2', hour='*/4')
 def job_scrap_kraken_ltc_usd():
     with scheduler.app.app_context():
         scrap_kraken('XLTCZUSD')
 
 
-@scheduler.task('cron', minute='3', hour="*/4")
+@scheduler.task('cron', minute='3', hour='*/4')
 def job_scrap_kraken_eth_btc():
     with scheduler.app.app_context():
         scrap_kraken('XETHXXBT')
 
 
-@scheduler.task('cron', minute='4', hour="*/4")
+@scheduler.task('cron', minute='4', hour='*/4')
 def job_scrap_kraken_ltc_btc():
     with scheduler.app.app_context():
         scrap_kraken('XLTCXXBT')
 
 
-@scheduler.task('cron', minute='5', hour="*/4")
+@scheduler.task('cron', minute='5', hour='*/4')
 def job_scrap_kraken_ltc_eth():
     with scheduler.app.app_context():
         scrap_kraken('LTCETH')
