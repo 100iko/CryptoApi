62091509840027b93f167683e05cd4cf442cba3d
Stoyko Stoykov <stoyko.stoykov@pulselive.com>
2021-01-25 22:25:11 +0200

    update crons

--- cron/cron.py
+++ cron/cron.py
@@ -1,44 +1,41 @@
 from flask_apscheduler import APScheduler
-import atexit
 
 from .jobs import scrap_kraken
 
 scheduler = APScheduler()
 
-atexit.register(lambda: scheduler.shutdown())
 
-
-@scheduler.task('cron', minute='0', hour='*/4')
+@scheduler.task('cron', second='0', minute='0', hour='*/3')
 def job_scrap_kraken_btc_usd():
     with scheduler.app.app_context():
         scrap_kraken('XXBTZUSD')
 
 
-@scheduler.task('cron', minute='1', hour='*/4')
+@scheduler.task('cron', second='10', minute='0', hour='*/3')
 def job_scrap_kraken_etg_usd():
     with scheduler.app.app_context():
         scrap_kraken('XETHZUSD')
 
 
-@scheduler.task('cron', minute='2', hour='*/4')
+@scheduler.task('cron', second='20', minute='0', hour='*/3')
 def job_scrap_kraken_ltc_usd():
     with scheduler.app.app_context():
         scrap_kraken('XLTCZUSD')
 
 
-@scheduler.task('cron', minute='3', hour='*/4')
+@scheduler.task('cron', second='30', minute='0', hour='*/3')
 def job_scrap_kraken_eth_btc():
     with scheduler.app.app_context():
         scrap_kraken('XETHXXBT')
 
 
-@scheduler.task('cron', minute='4', hour='*/4')
+@scheduler.task('cron', second='40', minute='0', hour='*/3')
 def job_scrap_kraken_ltc_btc():
     with scheduler.app.app_context():
         scrap_kraken('XLTCXXBT')
 
 
-@scheduler.task('cron', minute='5', hour='*/4')
+@scheduler.task('cron', second='50', minute='0', hour='*/3')
 def job_scrap_kraken_ltc_eth():
     with scheduler.app.app_context():
         scrap_kraken('LTCETH')
