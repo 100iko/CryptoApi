ba6c8de3ecd52a8e2f1b5820ee9b34b120b27505
Stoyko Stoykov <stoyko.stoykov@pulselive.com>
2021-01-27 17:31:35 +0200

    implement get count and missing data check

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
-from .candle import CandleList
+from .candle import CandleList, CandlesCount
 
 api = Api()
 
 api.add_resource(CandleList, '/candles/<string:pair>')
+api.add_resource(CandlesCount, '/candles/<string:pair>/count')
--- api/candle.py
+++ api/candle.py
@@ -2,19 +2,19 @@ from flask_restful import Resource, reqparse, fields, marshal_with, abort
 
 from models import candle_pairs
 
-candle_fields = {
-    'time': fields.Integer,
-    'open': fields.Float,
-    'high': fields.Float,
-    'low': fields.Float,
-    'close': fields.Float,
-    'vwap': fields.Float,
-    'volume': fields.Float,
-    'count': fields.Integer,
-}
-
 
 class CandleList(Resource):
+    response_format = {
+        'time': fields.Integer,
+        'open': fields.Float,
+        'high': fields.Float,
+        'low': fields.Float,
+        'close': fields.Float,
+        'vwap': fields.Float,
+        'volume': fields.Float,
+        'count': fields.Integer,
+    }
+
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('interval', type=int, default=1, location="args")
@@ -22,8 +22,10 @@ class CandleList(Resource):
         self.reqparse.add_argument('offset', type=int, default=0, location="args")
         super(CandleList, self).__init__()
 
-    @marshal_with(candle_fields)
-    def get(self, pair):
+    @marshal_with(response_format)
+    def get(self, pair: str):
+        pair = pair.upper()
+
         if pair is candle_pairs.keys():
             abort(400)
 
@@ -33,3 +35,30 @@ class CandleList(Resource):
 
         return candle_type.query.order_by(candle_type.time.desc()).offset(args['offset']).limit(
             args['limit']).all(), 200
+
+
+class CandlesCount(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('interval', type=int, default=1, location="args")
+        super(CandlesCount, self).__init__()
+
+    def get(self, pair: str):
+        pair = pair.upper()
+
+        if pair is candle_pairs.keys():
+            abort(400)
+
+        args = self.reqparse.parse_args()
+
+        candle_type = candle_pairs[pair]
+
+        count: int = candle_type.query.count()
+
+        first: int = candle_type.query.order_by(candle_type.time.asc()).first().time
+        last: int = candle_type.query.order_by(candle_type.time.desc()).first().time
+        expected: int = int((last - first) / 60) + 1
+
+        missing_data = count != expected
+
+        return {'count': count, 'missing_data': missing_data}, 200
