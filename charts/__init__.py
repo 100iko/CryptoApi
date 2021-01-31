09593d3c1185f988f080dea93096fde699db6dce
Stoyko Stoykov <stoyko.stoykov@pulselive.com>
2021-02-01 00:14:50 +0200

    implements intervals for the chart

--- charts/__init__.py
+++ charts/__init__.py
@@ -40,11 +40,20 @@ def init_dash(server):
                         className="m-2",
                         children=[dbc.Select(
                             id='interval',
-                            value='1m',
+                            value=30,
                             options=[
-                                {"label": '1m', "value": '1m'},
-                                {"label": '3m', "value": '3m'},
-                                {"label": '5m', "value": '5m'},
+                                {"label": '1m', "value": 1},
+                                {"label": '3m', "value": 3},
+                                {"label": '5m', "value": 5},
+                                {"label": '15m', "value": 15},
+                                {"label": '30m', "value": 30},
+                                {"label": '1h', "value": 60},
+                                {"label": '2h', "value": 60 * 2},
+                                {"label": '3h', "value": 60 * 3},
+                                {"label": '4h', "value": 60 * 4},
+                                {"label": '6h', "value": 60 * 6},
+                                {"label": '12h', "value": 60 * 12},
+                                {"label": '1d', "value": 60 * 24},
                             ]
                         )]
                     )], style={"display": "flex", "flexWrap": "wrap"}),
@@ -67,8 +76,9 @@ def init_dash_callbacks(app):
         if pair is None: return
         if interval is None: return
 
-        candle_type = unique_pairs[pair.upper()]
-        data = candle_type.query.order_by(candle_type.time.desc()).limit(24 * 60).all()
+        # candle_type = unique_pairs[pair.upper()]
+        # data = candle_type.query.order_by(candle_type.time.desc()).limit(24 * 60).all()
+        data = generate_intervals(pair, int(interval))
 
         time_periods = [datetime.fromtimestamp(x.time) for x in data]
 
@@ -97,7 +107,7 @@ def init_dash_callbacks(app):
             name='Vwap',
             fillcolor='orange',
             mode='markers',
-            marker=dict(color='orange', size=2),
+            marker=dict(color='orange', size=3),
             showlegend=False,
         )
 
@@ -106,7 +116,10 @@ def init_dash_callbacks(app):
         fig.add_trace(candle_data, secondary_y=True)
         fig.add_trace(vwap_data, secondary_y=True)
 
-        update_layout(fig, time_periods[120], time_periods[0] + timedelta(minutes=5), max(volumes) * 3)
+        update_layout(fig,
+                      time_periods[min(120, len(time_periods) - 1)],
+                      time_periods[0] + timedelta(minutes=int(interval) * 5),
+                      max(volumes) * 3)
 
         return fig
 
@@ -144,3 +157,32 @@ def init_dash_callbacks(app):
                 showgrid=True
             ),
         ))
+
+
+def generate_intervals(pair_type, interval: int):
+    candle_type = unique_pairs[pair_type.upper()]
+    candles = candle_type.query.order_by(candle_type.time.asc()).all()
+
+    while (candles[0].time / 60) % interval != 0:
+        candles.pop(0)
+
+    while len(candles) % interval != 0:
+        candles.pop(-1)
+
+    new_data = []
+    for i in range(0, len(candles), interval):
+        range_list = candles[i:i + interval]
+
+        new_time = range_list[0].time
+        new_open = range_list[0].open
+        new_high = max([x.high for x in range_list])
+        new_low = min([x.low for x in range_list])
+        new_close = range_list[-1].close
+        new_volume = sum([x.volume for x in range_list])
+        new_vwap = sum(x.vwap * x.volume for x in range_list) / new_volume if new_volume > 0 else 0
+        new_count = sum([x.count for x in range_list])
+
+        new_candle = candle_type(new_time, new_open, new_high, new_low, new_close, new_vwap, new_volume, new_count)
+        new_data.append(new_candle)
+
+    return new_data[::-1]
